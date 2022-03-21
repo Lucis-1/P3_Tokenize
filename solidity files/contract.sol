a7a5beaeb03d5a0ea700d0f436c53208f1f632bc
DanielT1994 <ahyperduck@gmail.com>
2022-03-21 18:46:20 +1100

    added timelock code

--- solidity files/contract.sol
+++ solidity files/contract.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.5.5;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/math/SafeMath.sol";
-
 /**
 
 */
@@ -21,6 +20,9 @@ contract EthBNB {
     uint256 last_withdraw_amount;
     address contract_owner;
     uint fee;
+    mapping(address => uint) public lockTime;
+    mapping(address => uint) public balances;
+
 
     function pay(address payable recipient, uint amount) public payable {
         require(recipient == bnb_owner, "wrong account");
@@ -31,17 +33,7 @@ contract EthBNB {
         last_deposit_amount = msg.value;
         balance = address(this).balance + fee;
     }
-    function withdraw(uint amount, address payable recipient) public {
-        require(balance == amount || balance > amount, "insufficient funds");
-        require(recipient == bnb_owner, "not owner of property");
-        if (last_to_withdraw != recipient) {
-            last_to_withdraw = recipient;
-        }
-        last_withdraw_amount = amount;
-        balance = address(this).balance - amount;
 
-        return msg.sender.transfer(amount);
-    }
     function refund (uint256 amount, address refundee) public payable {
         require (amount == fee, "not equal");
         require (refundee == payer, "not payer");
@@ -58,8 +50,54 @@ contract EthBNB {
         balance = address(this).balance;
         cut = balance/10;
     }
+    function buy_property(address payable recipient, uint amount) public payable {
+        require (recipient == bnb_owner, "not owner");
+        if (last_to_deposit != msg.sender){
+            last_to_deposit = msg.sender;
+        }
+        last_deposit_amount = msg.value;
+        balance = address(this).balance + amount;
+    }
+    function increaseLockTime(uint _secondsToIncrease) public {
+
+        // the add function below is from safemath and will take care of uint overflow
+        // if a call to add causes an error an error will be thrown and the call to the function will fail
+         lockTime[msg.sender] = lockTime[msg.sender].add(_secondsToIncrease);
+
+    }
+
+      
+
+    function withdraw() public {
+
+        // check that the sender has ether deposited in this contract in the mapping and the balance is >0
+        require(balances[msg.sender] > 0, "insufficient funds");
+
+        // check that the now time is > the time saved in the lock time mapping
+        require(block.timestamp > lockTime[msg.sender], "lock time has not expired");
+      
+
+        // update the balance
+        balances[msg.sender] = 0;
+    }
     function() external payable {}
 }
+
+/**     unneeded delete if needed
+function withdraw_timelocked(uint amount, address payable recipient) public {
+        require(balance == amount || balance > amount, "insufficient funds");
+        require(recipient == bnb_owner, "not owner of property");
+        if (last_to_withdraw != recipient) {
+            last_to_withdraw = recipient;
+        }
+        last_withdraw_amount = amount;
+        balance = address(this).balance - amount;
+
+        return msg.sender.transfer(amount);
+    }
+    **/
+
+
     
 
 
